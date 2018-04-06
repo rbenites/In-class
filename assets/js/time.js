28b57ba6b2a7e7326331402f70d63f75146381cb
rbenites <rbenites115@gmail.com>
2018-04-05 20:02:11 -0700

    fixed format issue on JS

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,37 +1,33 @@
+/*jshint esversion: 6 */
 /* BEGIN JAVASCRIPT*/
 var config = {
-    apiKey: "AIzaSyDiG1XYSQGN1t2PLA5hdlXdJsuwal2Mo44",
-    authDomain: "timecard-database.firebaseapp.com",
-    databaseURL: "https://timecard-database.firebaseio.com",
-    projectId: "timecard-database",
-    storageBucket: "timecard-database.appspot.com",
-    messagingSenderId: "241596015367"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDiG1XYSQGN1t2PLA5hdlXdJsuwal2Mo44",
+  authDomain: "timecard-database.firebaseapp.com",
+  databaseURL: "https://timecard-database.firebaseio.com",
+  projectId: "timecard-database",
+  storageBucket: "timecard-database.appspot.com",
+  messagingSenderId: "241596015367"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
-    var eName = "";
-    var eRole = "";
-    var eYr = "";
-    var eRate = "";
-    var mRate = "";
-    var totBilled = "";
+var database = firebase.database();
+var eName = "";
+var eRole = "";
+var eYr = "";
+var eRate = "";
+var mRate = "";
+var totBilled = "";
 
-  $("#submit").on("click", function (){
-    event.preventDefault();
-    eName = $("#eName").val().trim();
-    eRole = $("#eRole").val().trim();
-    eYr = $("#eYr").val().trim();
-    eRate = $("#eRate").val().trim();
-    
-
-
-
-    })
+$("#submit").on("click", function () {
+  event.preventDefault();
+  eName = $("#eName").val().trim();
+  eRole = $("#eRole").val().trim();
+  eYr = $("#eYr").val().trim();
+  eRate = $("#eRate").val().trim();
 
 
 
-    
-  })
+
+});
 
 /*END JAVASCRIPT*/
